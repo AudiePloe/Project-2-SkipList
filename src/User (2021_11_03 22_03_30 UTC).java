f0fc77bdce0f03f39889fbf8a67f2cb3aa54a443
Audie Ploe <47163769+AudiePloe@users.noreply.github.com>
2025-09-02 10:50:43 -0500

    Update User

--- src/User (2021_11_03 22_03_30 UTC).java
+++ src/User (2021_11_03 22_03_30 UTC).java
@@ -3,51 +3,35 @@
 
 public class User // stores the user data
 {
+    public String SSN;
+    public String LastName;
+    public String FirstName;
+    
+    User()
+    {
+        SSN = null;
+        LastName = null;
+        FirstName = null;
+    }
 
-	public String SSN;
-	public String LastName;
-	public String FirstName;
-
-
-
-	User()
-	{
-
-		SSN = null;
-		LastName = null;
-		FirstName = null;
-
-	}
-
-	User(String s, String l, String f)
-	{
-		SSN = s;
-		LastName = l;
-		FirstName = f;
-
-	}
+    User(String s, String l, String f)
+    {
+        SSN = s;
+        LastName = l;
+        FirstName = f;
+    }
 
 
-	User getUserFromTxt(String s) // reads a string from txt and returns user from it
-	{
+    User getUserFromTxt(String s) // reads a string from txt and returns user from it
+    {
+        User tmpUser = new User();
 
-		User tmpUser = new User();
+        String parts[] = s.split(";"); // splits the inputed string by ; marks
 
-		String parts[] = s.split(";"); // splits the inputed string by ; marks
+        tmpUser.SSN = parts[0];
+        tmpUser.LastName = parts[1];
+        tmpUser.FirstName = parts[2];
 
-		tmpUser.SSN = parts[0];
-		tmpUser.LastName = parts[1];
-		tmpUser.FirstName = parts[2];
-
-		return tmpUser;
-	}
-
-	void printUser() // prints out usr data
-	{
-		System.out.println(SSN + " " + LastName + " " + FirstName + "\n");
-	}
-
-
-
-
+        return tmpUser;
+    }
 }
