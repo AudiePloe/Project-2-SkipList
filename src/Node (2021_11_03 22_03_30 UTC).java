59ffdf29c9c23c949881ed2d3f4b3649ab7f30cc
Audie Ploe <47163769+AudiePloe@users.noreply.github.com>
2025-09-02 10:49:32 -0500

    Update Node

--- src/Node (2021_11_03 22_03_30 UTC).java
+++ src/Node (2021_11_03 22_03_30 UTC).java
@@ -1,50 +1,17 @@
 
-public class Node // node is a storage object that stores a user and knows what other nodes are in
-					// front of it
-{
-	User user;
+class Node {
+    User user;
+    Node[] forward; // Array of pointers for different levels
 
-	Node next100;
-	Node next10;
-	Node next1;
-	Node next;
-
-
-	Node()
-	{
-
-		user = null;
-
-		next100 = null;
-		next10 = null;
-		next1 = null;
-		next = null;
-
-	}
+    public Node(User user, int level)
+    {
+        this.user = user;
+        this.forward = new Node[level + 1];
+    }
 
 
-	public Node(User u)
-	{
-		user = u;
-
-		next100 = null;
-		next10 = null;
-		next1 = null;
-		next = null;
-	}
-
-
-	public Node(String ssn) // used when setting up the skip list
-	{
-		user = new User();
-
-		user.SSN = ssn;
-
-		next100 = null;
-		next10 = null;
-		next1 = null;
-		next = null;
-	}
-
-
+    @Override public String toString()
+    {
+        return (user.SSN + " " + user.FirstName + " " + user.LastName);
+    }
 }
