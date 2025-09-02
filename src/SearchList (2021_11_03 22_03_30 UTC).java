a2c4abd16cb390b71858336b6e7653ab2dd6c470
Audie Ploe <47163769+AudiePloe@users.noreply.github.com>
2025-09-02 10:48:58 -0500

    Update SearchList to correctly use a SkipList

--- src/SearchList (2021_11_03 22_03_30 UTC).java
+++ src/SearchList (2021_11_03 22_03_30 UTC).java
@@ -1,154 +1,126 @@
 //
 // Audie Ploe
-// 10/02/2021
+// 10/02/2021 -- Revised 09/02/2025
 //
-
-
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class SearchList extends IOmethods
+public class SearchList
 {
 
-	static SkipList userList = new SkipList(); // the skip list that holds all the nodes
+    static SkipList userList = new SkipList(); // the skip list that holds all the nodes
 
+    public static void main(String[] a) throws IOException
+    {
 
+        long timeS = System.currentTimeMillis(); // takes starting time in milliseconds
+        long timeE = 0;
 
+        System.out.println("LOADING-DATABASE----------------------------------");
 
+        userList.compile(); // creates the list
 
-	public static void main(String a[]) throws IOException
-	{
+        timeE = System.currentTimeMillis(); // takes ending time in milliseconds
 
-		long timeS = System.currentTimeMillis(); // takes starting time in milliseconds
-		long timeE = 0;
+        System.out.print("\n\nData loaded in " + ((timeE - timeS) / 1000) + " seconds.\n\n"); // print out how many seconds it took
 
-		System.out.println("LOADING-DATABASE----------------------------------");
+        menu(); // the user input part
 
-		userList.compile(); // creates the list
+        System.out.println("\nClosed");
 
-		timeE = System.currentTimeMillis(); // takes ending time in milliseconds
+    }
 
-		System.out.print("\n\nData loaded in " + ((timeE - timeS) / 1000) + " seconds.\n\n"); // print out how many seconds it took
+    static void menu() throws FileNotFoundException // used for getting user input.
+    {
+        Scanner reader = new Scanner(System.in);
 
-		menu(); // the user input part
+        String tmpString = ""; // used as storage for user input
+        int num = 0; // used as storage for user input
 
-		System.out.println("\nClosed");
+        do // loops asking the user what they want to do
+        {
+            System.out.println("\nWelcome to the user database. To look up a user type 1. To add a new user type 2. To exit type 3. \n");
 
-	}
+            num = reader.nextInt();
 
+            if (num == 1) // If looking up user
+            {
+                do
+                {
+                    System.out.print("\n\nPlease enter the SSN of the user you are looking for: ");
 
+                    tmpString = reader.next();
 
+                } while (!isNumeric(tmpString));
 
+                userList.logSearch(tmpString); // look up user using skip list
+            }
 
-	static void menu() throws FileNotFoundException // used for getting user input.
-	{
-		Scanner reader = new Scanner(System.in);
+            else if (num == 2) // or if add new user
+            {
+                String ssn = "";
+                String last = "";
+                String first = "";
 
-		String tmpString = ""; // used as storage for user input
-		int num = 0; // used as storage for user input
 
-		do // loops asking the user what they want to do
-		{
-			num = 0;
+                System.out.println("\n\nPlease enter the SSN of the user you are adding: \n");
 
-			System.out.println("\nWelcome to the user database. To look up a user type 1. To add a new user type 2. To exit type 3. \n");
+                ssn = reader.next();
 
-			num = reader.nextInt();
+                System.out.println("\n\nPlease enter the last name of the user you are adding: \n");
 
-			if (num == 1) // if look up user
-			{
+                last = reader.next();
 
-				do
-				{
+                System.out.println("\n\nPlease enter the first name of the user you are adding: \n");
 
-					System.out.println("\n\nPlease enter the SSN of the user you are looking for: ");
+                first = reader.next();
 
-					tmpString = reader.next();
+                addUser(ssn, last, first);
 
-				} while (!isNumeric(tmpString));
+                System.out.println("\n\nUser has been added: \n\n");
 
+                userList.logSearch(ssn);
 
-				System.out.println("\nTo search with log type 1, to search linearly type 2\n");
+            }
 
-				num = reader.nextInt();
 
+        } while (num != 3); // loop until user enters 3 to close
 
-				if (num == 2) // look up user using bottom of list (linearly)
-					userList.linearSearch(tmpString);
-				else
-					userList.logSearch(tmpString); // look up user using skip list
 
+        reader.close();
 
-			}
+    }
 
-			else if (num == 2) // or if add new user
-			{
-				String ssn = "";
-				String last = "";
-				String first = "";
 
+    static void addUser(String s, String l, String f)
+    {
+        // 1. Create the new User object.
+        User newUser = new User(s, l, f);
 
-				System.out.println("\n\nPlease enter the SSN of the user you are adding: \n");
+        // 2. Determine the random level for the new node.
+        int newLevel = userList.randomLevel();
 
-				ssn = reader.next();
+        // 3. Create the new Node with the correct level.
+        Node newNode = new Node(newUser, newLevel);
 
-				System.out.println("\n\nPlease enter the last name of the user you are adding: \n");
+        // 4. Add the new node to the skip list.
+        userList.add(newNode, newLevel);
+    }
 
-				last = reader.next();
-
-				System.out.println("\n\nPlease enter the first name of the user you are adding: \n");
-
-				first = reader.next();
-
-				addUser(ssn, last, first);
-
-				System.out.println("\n\nUser has been added: \n\n");
-
-				userList.logSearch(ssn);
-
-			}
-
-
-		} while (num != 3); // loop until user enters 3 to close
-
-
-		reader.close();
-
-	}
-
-
-
-	static void addUser(String s, String l, String f) throws FileNotFoundException // add a user from string input
-	{
-
-		userList.add(new Node(new User(s, l, f)), true);
-
-	}
-
-
-
-
-
-	public static boolean isNumeric(String str) // checks to see if ssn is a number or not
-	{
-
-		try
-		{
-
-			Double.parseDouble(str); // if it can parse to a double then true
-			return true;
-
-		}
-
-		catch (NumberFormatException e)
-		{
-			return false;
-		}
-
-	}
-
-
-
+    public static boolean isNumeric(String str) // checks to see if ssn is a number or not
+    {
+        try
+        {
+            Double.parseDouble(str); // if it can parse to a double then true
+            return true;
+        }
+
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+
+    }
 }
